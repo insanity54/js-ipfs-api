33d641eaee0d7a13b58cad005660c13102fd5dc4
dignifiedquire <dignifiedquire@gmail.com>
2016-03-21 12:51:11 +0100

    refactor: Move testing and linting into digified.js

--- tasks/test.js
+++ tasks/test.js
@@ -1,66 +1,10 @@
 'use strict'
 
 const gulp = require('gulp')
-const Server = require('karma').Server
-const $ = require('gulp-load-plugins')()
-const runSequence = require('run-sequence')
-const path = require('path')
-
-const config = require('./config')
 
 require('./daemons')
 
-// Workaround gulp not exiting if there are some
-// resources not freed
-const exitOnFail = (err) => {
-  if (err) {
-    process.exit(1)
-  } else {
-    process.exit(0)
-  }
-}
-
-gulp.task('test', (done) => {
-  runSequence(
-    'test:node',
-    'test:browser',
-    exitOnFail
-  )
-})
-
-gulp.task('test:node', (done) => {
-  runSequence(
-    'daemons:start',
-    'mocha',
-    'daemons:stop',
-    exitOnFail
-  )
-})
-
-gulp.task('test:browser', (done) => {
-  runSequence(
-    'daemons:start',
-    'karma',
-    'daemons:stop',
-    exitOnFail
-  )
-})
-
-gulp.task('mocha', () => {
-  return gulp.src([
-    'test/setup.js',
-    'test/**/*.spec.js'
-  ])
-    .pipe($.mocha({
-      timeout: config.webpack.dev.timeout
-    }))
-})
-
-gulp.task('karma', (done) => {
-  new Server({
-    configFile: path.join(__dirname, '/../karma.conf.js'),
-    singleRun: true
-  }, (code) => {
-    done(code > 0 ? 'Some tests are failing' : undefined)
-  }).start()
-})
+gulp.task('test:node:before', ['daemons:start'])
+gulp.task('test:node:after', ['daemons:stop'])
+gulp.task('test:browser:before', ['daemons:start'])
+gulp.task('test:browser:after', ['daemons:stop'])
